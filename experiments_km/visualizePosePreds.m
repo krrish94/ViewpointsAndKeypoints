f31836d00dc0fe21b2c95ecc65b2d072804ebc02
Krishna Murthy <krrish94@gmail.com>
2016-06-28 10:18:09 +0530

    Visualizing Vps Preds

---
+++ experiments_km/visualizePosePreds.m
@@ -0,0 +1,98 @@
+% We're interested only in the 'car' class
+class = 'car';
+
+% Load pose prediction features, if they don't already exist
+if ~exist('feat', 'var')
+    load(fullfile(cachedir, 'rcnnPredsVps', 'vggJointVps', class), 'feat');
+end
+% The variable 'feat' now contains pose features.
+
+% Load train/test data, if not already loaded
+if ~exist('data', 'var')
+    data = load(fullfile(cachedir,'evalSets',class));
+end
+
+% Generate evaluation sets from the data loaded above
+[trainLabels,testLabels,trainFeats,testFeats] = generateEvalSetData(data);
+
+% Predict poses on test set (alpha is the weight parameter, used to weight
+% coarse and fine pose features; nHypotheses is the number of hypotheses to
+% consider while predicting pose)
+alphaOpt = params.alpha;
+nHypotheses = params.nHypotheses;
+testPreds = poseHypotheses(testFeats, nHypotheses, alphaOpt);
+% Since we consider only one hypothesis, load it
+testPreds = testPreds{1};
+
+
+% Generate a sample of 1 image to test the visualizeRotations function
+
+% Index (indices) of the current image(s) we're considering
+% curIdx = 1:10;
+curIdx = 11:20;
+
+% Ground truth for the current instance(s)
+gt = testLabels(curIdx,:);
+% Prediction for the current instance(s)
+prediction = testPreds(curIdx,:);
+% Data struct for the current instance(s)
+for i = 1:length(curIdx)
+    dataStruct_temp.voc_ids{i} = data.test.voc_ids{curIdx(i)};
+    dataStruct_temp.bboxes(i,:) = data.test.bboxes(curIdx(i),:);
+end
+
+% Run the 'visualizeRotations' function. Not so useful, as this just plots
+% the test image, with the predicted and ground truth azimuths, as well as
+% their ratio.
+% visualizeRotations(gt, prediction, dataStruct_temp, 'euler', 1);
+
+%% Visualize CAD models of the predictions
+
+% Path to directory containing CAD models
+CADPath = fullfile(PASCAL3Ddir,'CAD',class);
+% Load the CAD models available for the current class
+cad = load(CADPath);
+cad = cad.(class);
+% Get the vertices and faces from the first CAD model
+vertices = cad(1).vertices;
+faces = cad(1).faces;
+
+% Rotation matrix commonly applied to all instances (why ???)
+rotX = diag([1 -1 -1]);
+
+% For each prediction (instance)
+for i = 1:length(curIdx)
+    % Display the voc_id of the current instance
+    disp(dataStruct_temp.voc_ids{i});
+    % Read in the image
+    im = imread(fullfile(pascalImagesDir, [dataStruct_temp.voc_ids{i}, '.jpg']));
+    
+    % In one subplot, plot the image and the corresponding bbox
+    subplot(1,2,1);
+    showboxes(im, dataStruct_temp.bboxes(i,:));
+    axis image;
+    
+    % Get the Euler angles for the current prediction
+    euler = testPreds(i,:);
+    
+    % Compute the transformation from the CAD frame to the viewpoint
+    R = angle2dcm(euler(1), euler(2)-pi/2, -euler(3),'ZXZ');
+    R = rotX*R';
+    % R = eye(3);
+    % R = [1,0,0;0,0,-1;0,1,0]; % pi/2 about X
+    % R = diag([1, -1, -1]); % pi about X
+    % R = [cos(pi/4), -sin(pi/4), 0; sin(pi/4), cos(pi/4), 0; 0, 0, 1]*diag([1, -1, -1]); % pi about X, and pi/2 about Z
+    % Apply the transformation to the vertices of the CAD model
+    verticesP = R*vertices';
+    verticesP = verticesP';
+    
+    % In the other subplot, plot the CAD model, transformed according to
+    % the estimated viewpoint of the current instance
+    subplot(1,2,2);
+    trisurf(faces,verticesP(:,1),verticesP(:,2),verticesP(:,3));axis equal;view(0,-90);
+    
+    % Wait for key press from the user
+    pause();
+    % Close the current figure. Opens a new figure for the next detection.
+    close all;
+end
--- visualization/visualizeRotations.m
+++ visualization/visualizeRotations.m
@@ -1,29 +1,59 @@
 function [] = visualizeRotations(gt,prediction,data,encoding,scale)
 %VISUALIZEROTATIONS Summary of this function goes here
 %   Detailed explanation goes here
+
+
+% Declaring global variables
 globals;
+
+% Decode the current predictions to Euler angles
 eulersPred = decodePose(prediction,encoding);
+% Decode the ground-truth predictions to Euler angles
 eulersGt = decodePose(gt,encoding);
+
+% Encode the current predictions from Euler angles to rotation matrices
 rotsPred = encodePose(eulersPred,'rot');
+% Encode the ground-truth predictions from Euler angles to rotation mats
 rotsGt = encodePose(eulersGt,'rot');
 
-for i=1:size(gt,1)
+% For each prediction (in the ground-truch annotations passed)
+for i = 1:size(gt,1)
+    % Reshape the predicted and ground-truth rotation matrices to 3 x 3
     rotPred = reshape(rotsPred(i,:),3,3);
     rotGt = reshape(rotsGt(i,:),3,3);
-    predVec = vrrotmat2vec(rotPred);predVec = predVec(1:3)*predVec(4)*scale;
-    gtVec = vrrotmat2vec(rotGt);gtVec = gtVec(1:3)*gtVec(4)*scale;
-    im = imread(fullfile(PASCAL_DIR,[data.voc_ids{i} '.jpg']));
+    
+    % Convert the predicted vector from a rotation matrix to an axis-angle
+    % form.
+    predVec = vrrotmat2vec(rotPred);
+    predVec = predVec(1:3)*predVec(4)*scale;
+    % Convert the ground-truth annotation from a rotation matrix to an
+    % axis-angle form
+    gtVec = vrrotmat2vec(rotGt);
+    gtVec = gtVec(1:3)*gtVec(4)*scale;
+    
+    % Read in the current image from the pascal dataset
+    im = imread(fullfile(pascalImagesDir,[data.voc_ids{i} '.jpg']));
+    % Retrive the current instance's bounding box
     bbox = data.bboxes(i,:);
-    xMean = (bbox(1)+bbox(3))/2;yMean = (bbox(2)+bbox(4))/2;
+    % Get the center of the bounding box
+    xMean = (bbox(1)+bbox(3))/2;
+    yMean = (bbox(2)+bbox(4))/2;
+    
+    % Initialize a new plot
     figure();
+    
+    % Plot the image onto the axes
     warp(im);hold on;
+    % 
     quiver3(xMean,yMean,0,predVec(1),predVec(2),predVec(3),'g','LineWidth',3);hold on;
     quiver3(xMean,yMean,0,gtVec(1),gtVec(2),gtVec(3),'r','LineWidth',3);hold on;
+    
+    % 
     e1 = norm(logm(rotGt*rotPred'))*sqrt(2)*180/pi;
     e2 =  1/scale*norm(gtVec-predVec)*180/pi;
     
     title([num2str(e1) , ' ',num2str(e2), ' ' num2str(e1/e2)]);axis equal;
-    pause();close all;
+    pause(); close all;
 end
 
 end
